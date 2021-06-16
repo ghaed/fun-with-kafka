ef8e52d0fb228fd774fe8ddefd5d98c857892e72
ghaed <mahaghaed@gmail.com>
2021-06-16 00:34:21 -0700

    - Made producer high throughput

--- tutorial2/TwitterProducer.java
+++ tutorial2/TwitterProducer.java
@@ -97,6 +97,11 @@ public class TwitterProducer {
         properties.setProperty(ProducerConfig.RETRIES_CONFIG,  Integer.toString(Integer.MAX_VALUE));
         properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
 
+        // High throughput producer (at the expense of a bit of latency and CPU)
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32*1024));
+
         // create the producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
         return producer;
